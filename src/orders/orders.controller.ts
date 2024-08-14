4b0854eb4d5f8aaf0b4a66747fea4b1be1d98f30
Jesús <jesusguerreropaniagua6@gmail.com>
2024-08-14 14:24:47 +0200

    feat: started orders ms implementation

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  NotImplementedException,
+  ParseUUIDPipe,
+} from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+
+import { OrdersService } from "./orders.service";
+
+import { CreateOrderDto } from "./dto/create-order.dto";
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern("createOrder")
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern("findAllOrders")
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern("findOneOrder")
+  findOne(@Payload("id", ParseUUIDPipe) id: string) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern("changeOrderStatus")
+  changeOrderStatus() {
+    // return this.ordersService.changeStatus();
+    throw new NotImplementedException();
+  }
+}
